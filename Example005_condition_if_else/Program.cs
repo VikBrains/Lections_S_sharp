db41fbc61727489516d31617821bc5a29dd9ea97
VikBrains <zi3gglhdjfk9@mail.ru>
2023-06-11 15:54:07 +0300

    add Example005_condition_if_else with error

---
+++ Example005_condition_if_else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Input User name: ");
+string username = Console.ReadLine();
+
+if(username.ToLower == "mary")
+{
+    Console.WriteLine("Ура, это же МАША!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
