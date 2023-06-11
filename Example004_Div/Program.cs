96a19639c05f5843b14f7f3fc7b451d78df9b0d1
VikBrains <zi3gglhdjfk9@mail.ru>
2023-06-11 12:42:47 +0300

    add Example004_Div

---
+++ Example004_Div/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+double numberA = 12;
+double numberB = 5;
+// int result = numberA + numberB;
+Console.WriteLine(numberA / numberB);
