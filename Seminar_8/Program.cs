57dbf0aac4055edc8ca88121ba2fe765115eb017
VikBrains <zi3gglhdjfk9@mail.ru>
2023-07-04 23:00:36 +0300

    add seminar 8

---
+++ Seminar_8/Program.cs
@@ -0,0 +1,56 @@
+﻿// Задача 53: Задайте двумерный массив. Напишите программу, которая поменяет местами первую и последнюю строку массива.
+
+int[,] array = new int [5,5];
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+         array[i,j] = new Random().Next(1,10);
+         Console.Write(array[i,j] + " ");
+    }
+    Console.WriteLine();
+}
+
+
+
+int [,] ChangeRows (int[,] inputArray) 
+
+{
+    int [,] OutputMatrix = new int [inputArray.GetLength(0), inputArray.GetLength(1)];
+
+    for (int i = 0; i < inputArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inputArray.GetLength(1); j++)
+        {
+            OutputMatrix[i,j] = inputArray[i,j];
+        }
+    }
+
+    // Меняем элементы последней строки на элементы 1-ой
+    for (int i = 0; i < inputArray.GetLength(0); i++)
+    {
+        OutputMatrix[inputArray.GetLength(0) - 1, i] = inputArray[0,i];
+    }
+
+    // Меняем элементы первой строки на элементы последней
+    for (int i = 0; i < inputArray.GetLength(0); i++)
+    {
+        OutputMatrix[0,i] = inputArray[OutputMatrix.GetLength(0) - 1,i];
+    }
+    return OutputMatrix;
+}
+
+
+void printMatrix (int[,] input_array) { 
+    System.Console.WriteLine();  
+    System.Console.Write("Your flipped array is:");
+    for (int column = 0; column < input_array.GetLength(0); column++){
+        System.Console.WriteLine();
+        for (int row = 0; row < input_array.GetLength(1); row++){
+            System.Console.Write(input_array[column, row] + " ");
+        }
+    }
+}
+
+printMatrix (ChangeRows(array));
---
+++ Seminar__8_T55/Program.cs
@@ -0,0 +1,51 @@
+﻿/* Задача 55: Задайте двумерный массив. 
+Напишите программу, которая заменяет строки на столбцы. 
+В случае, если это невозможно, программа должна вывести сообщение для пользователя. */
+
+int[,] array = new int [5,5];
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+         array[i,j] = new Random().Next(1,10);
+         Console.Write(array[i,j] + " ");
+    }
+    Console.WriteLine();
+}
+
+int [,] ChangeRows (int[,] inputArray) 
+
+{
+    int [,] OutputMatrix = new int [inputArray.GetLength(0), inputArray.GetLength(1)];
+    if (inputArray.GetLength(0) == inputArray.GetLength(1))
+    {
+        
+
+        for (int i = 0; i < inputArray.GetLength(0); i++)
+        {
+            for (int j = 0; j < inputArray.GetLength(1); j++)
+            {
+                OutputMatrix[j,i] = inputArray[i,j];
+            }
+        }
+    }
+    else
+    {
+        System.Console.WriteLine("operation impossible, matrix isn't a square");
+    }
+    return OutputMatrix;
+}
+
+void printMatrix (int[,] input_array) { 
+    System.Console.WriteLine();  
+    System.Console.Write("Your flipped array is:");
+    for (int column = 0; column < input_array.GetLength(0); column++){
+        System.Console.WriteLine();
+        for (int row = 0; row < input_array.GetLength(1); row++){
+            System.Console.Write(input_array[column, row] + " ");
+        }
+    }
+}
+
+printMatrix (ChangeRows(array));
